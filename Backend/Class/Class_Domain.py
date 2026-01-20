80662c3afd7c9e164b43e930d7461ada4f426a63
py18uam <py18uam@eduvaud.ch>
2026-01-20 13:43:30 +0100

    Amélioration des Classes

--- Backend/Class/Class_Domain.py
+++ Backend/Class/Class_Domain.py
@@ -2,26 +2,17 @@
 Nom du fichier : Class_Domain.py
 Auteur : Joel Cunha Faria
 Date de création : 16.01.2026
-Date de modification : 16.01.2026
+Date de modification : 19.01.2026
 """
 from sqlalchemy import Column, Integer, String, Date
 from Backend.DB.db_schema import Base
 
 class Domain(Base):
-    __tablename__ = "User"
+    __tablename__ = "domains"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     information = Column(String(500), nullable=False)
-
-    def __init__(self, **kwargs):
-        """
-            Initialize a Person instance
-            Accepts any keyword arguments and sets firstname and lastname
-            This allows creation for CRUD operations
-        """
-        self.name = kwargs.get("name")
-        self.information = kwargs.get("information")
 
     def __repr__(self):
         """
