e83e5d42763a12be0c5bb32b90b47f789c7f2e36
onfire1003 <py18uam@eduvaud.ch>
2026-01-21 13:49:47 +0100

    fichier pour créer la DB

--- Backend/DB/init_db.py
+++ Backend/DB/init_db.py
@@ -2,7 +2,7 @@
 Nom du fichier : init_db.py
 Auteur : Joel Cunha Faria
 Date de création : 19.01.2026
-Date de modification : 19.01.2026
+Date de modification : 20.01.2026
 """
 from Backend.DB.db_schema import engine, Base
 
@@ -11,7 +11,7 @@ from Backend.Class.Class_User import User
 from Backend.Class.Class_Domain import Domain
 
 def init_db():
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
     init_db()
