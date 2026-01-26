9e166014571b10fa02447f44fcb4ea6d2db48a73
onfire1003 <py18uam@eduvaud.ch>
2026-01-26 12:09:35 +0100

    login d'un utilisateur

--- Backend/Services/auth_service.py
+++ Backend/Services/auth_service.py
@@ -1,5 +1,5 @@
 """
-Nom du fichier : seed_domains.py
+Nom du fichier : auth_service.py
 Auteur : Joel Cunha Faria
 Date de création : 23.01.2026
 Date de modification : 23.01.2026
@@ -8,6 +8,7 @@ from sqlalchemy.orm import Session
 from Backend.DB.db_schema import engine
 from Backend.Class.Class_User import User
 from sqlalchemy import select
+from datetime import datetime
 
 class AuthService:
 
@@ -27,5 +28,35 @@ class AuthService:
 
             if user.password != password:
                 return False, "Mot de passe incorrect"
+
+            return True, user
+
+    @staticmethod
+    def signup(username: str, email: str, birthdate: str, password: str):
+        with Session(engine) as session:
+
+            # Vérifier username ou email déjà existant
+            stmt = select(User).where(
+                (User.username == username) | (User.email == email)
+            )
+            if session.execute(stmt).scalar_one_or_none():
+                return False, "Utilisateur ou email déjà utilisé"
+
+            try:
+                birthdate = datetime.strptime(birthdate, "%Y-%m-%d").date()
+            except ValueError:
+                return False, "Format de date invalide (YYYY-MM-DD)"
+
+            user = User(
+                username=username,
+                email=email,
+                birthdate=birthdate,
+                password=password,
+                role="membre"
+            )
+
+            session.add(user)
+            session.commit()
+            session.refresh(user)
 
             return True, user
