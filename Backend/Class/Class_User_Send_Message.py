746e07a6d2a0e37ef172c89cc47bfbe0bbf2eafe
David <hystuturo@gmail.com>
2026-01-28 15:07:24 +0100

    Ajout de la gestion d'envoi des messages

--- Backend/Class/Class_User_Send_Message.py
+++ Backend/Class/Class_User_Send_Message.py
@@ -1,1 +1,20 @@
+"""
+Nom du fichier : Class_User_Send_Message.py
+Auteur : David Vilela
+Date de création : 26.01.2026
+Date de modification : 28.01.2026
+"""
 
+from sqlalchemy import Column, Integer, ForeignKey
+from Backend.DB.db_schema import Base
+
+class UserSendMessage(Base):
+    __tablename__ = "users_send_messages"
+
+    id = Column(Integer, primary_key=True)
+
+    users_id = Column(Integer, ForeignKey("users.id"))
+    messages_id = Column(Integer, ForeignKey("messages.id"))
+
+    def __repr__(self):
+        return f"user_id: {self.users_id}, message_id: {self.messages_id}"
