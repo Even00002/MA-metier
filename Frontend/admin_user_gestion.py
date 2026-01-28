00ba6dc0d383f3803567063789c20f47e624717c
onfire1003 <py18uam@eduvaud.ch>
2026-01-28 09:17:47 +0100

    Modification de la gestion des users pour qu'ils s'affichent automatiquement en prenant les données depuis la DB

--- Frontend/admin_user_gestion.py
+++ Frontend/admin_user_gestion.py
@@ -2,7 +2,7 @@
 Nom du fichier : admin_user_gestion.py
 Auteur : Furkan Yilmaz
 Date de création : 21.01.2026
-Dernier changement : 27.01.2026
+Dernier changement : 28.01.2026
 """
 
 import customtkinter as ctk
